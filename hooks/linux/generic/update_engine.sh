eccacb299de186c85536c675396fa9740ef59938
Ryan Sturmer <ryansturmer@gmail.com>
2015-12-15 05:28:17 +0000

    Fix hook further

--- hooks/linux/generic/update_engine.sh
+++ hooks/linux/generic/update_engine.sh
@@ -1,11 +1,22 @@
-#!/bin/bash
+#!/bin/bash -e
 
+echo "Stopping the engine..."
 systemctl stop fabmo
+
+echo "Fetching new versions..."
 cd /fabmo/engine
 git fetch origin --tags
 git checkout master
 git pull
+
+echo "Updating to version $1..."
 git checkout $1
+
+echo "Installing dependencies..."
 npm install --production
+
+echo "Clearing the approot..."
 rm -rf /opt/fabmo/approot
+
+echo "Restarting the engine..."
 systemctl start fabmo
