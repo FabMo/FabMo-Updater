b9c26e414c2fd0e693b6cc6629f22362495a76c3
Ryan Sturmer <ryansturmer@gmail.com>
2016-01-12 19:14:33 +0000

    Fix logging

---
+++ routes/log.js
@@ -0,0 +1,33 @@
+var log = require('../log');
+
+/**
+ * @apiGroup Log
+ * @api {get} /log Get log
+ * @apiDescription Get the contents of the debug log
+ */
+var getLog = function(req, res, next) {
+  body = log.getLogBuffer();
+  res.setHeader('content-type', 'text/plain');
+  res.send(body);
+};
+
+/**
+ * @apiGroup Log
+ * @api {delete} /log Clear log
+ * @apiDescription Clear the debug log.
+ * @apiSuccess {String} status `success`
+ * @apiSuccess {Object} data null
+ */
+var clearLog = function(req, res, next) {
+    var answer = {
+        status:"success",
+        data : null
+    }
+    log.clearLogBuffer();
+    res.json(answer);
+};
+
+module.exports = function(server) {
+  server.get('/log', getLog);
+  server.del('/log', clearLog)
+};
