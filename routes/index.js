cd03e71c20948fe7720cd545763a22c67419e638
tedh-shopbot <ted@shopbottools.com>
2021-11-20 09:13:17 -0800

    fixed restify naming in routes\index.js

--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,8 @@ module.exports = function(server) {
 
 	// Define a route for serving static files
 	// This has to be defined after all the other routes, or it plays havoc with things
-	server.get(/.*/, restify.serveStatic({
+////##	server.get(/.*/, restify.serveStatic({
+	server.get(/.*/, restify.plugins.serveStatic({
 		directory: './static',
 		default: 'index.html'
 	}));
