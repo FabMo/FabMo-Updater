3ea3abb27664a1b55466cd9e7a7dfea78b71b951
tedh-shopbot <ted@shopbottools.com>
2021-11-20 09:34:38 -0800

    more on restify

--- routes/index.js
+++ routes/index.js
@@ -57,9 +57,12 @@ module.exports = function(server) {
 	// Define a route for serving static files
 	// This has to be defined after all the other routes, or it plays havoc with things
 ////##	server.get(/.*/, restify.serveStatic({
-	server.get(/.*/, restify.plugins.serveStatic({
+////##		directory: './static',
+////##		default: 'index.html'
+	server.get('/.*/', restify.plugins.serveStatic({
 		directory: './static',
-		default: 'index.html'
+		default: 'index.html',
+		appendRequestPath: false
 	}));
 
 };
