f42a0991c7c2608d1ac00cf6f6027a4964ab534f
Brian Owen <brian.owen@shopbottools.com>
2025-05-27 07:40:24 -0700

    Updated RegEx in get versions to look for double digit subversions.

--- hooks/linux/raspberry-pi/index.js
+++ hooks/linux/raspberry-pi/index.js
@@ -2,7 +2,7 @@ exports.get_versions = function(err, stdout, stderr, callback) {
 	if(err) {
 		return callback(err, new Error(stderr));
 	} 
-	var tag_re = /^([0-9a-f]+)\s+refs\/tags\/(v\d\.\d\.\d)$/i
+	var tag_re = /^([0-9a-f]+)\s+refs\/tags\/(v\d\.\d\.\d+)$/i
 	var lines = stdout.split('\n');
 	var retval = [];
 	lines.forEach(function(line) {
